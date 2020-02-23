4b054e8afb027e9a089a69fd0a00ab5ea2c1171b
Nabeel hussain shah <hamdani.no02@gmail.com>
2020-02-23 14:47:25 +0500

    our goal

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/safety',(req,response)=>
+{
+    fs.readFile('./html/our goal/safety.html', function (err, html) {
+        if (err) {
+            throw err;
+        }
+        else{
+            response.writeHeader(200, {"Content-Type": "text/html"});
+            response.write(html);
+            response.end();
+        }
+        });
+
+});
+
+app.get('/efficiency',(req,response)=>
+{
+    fs.readFile('./html/our goal/efficiency.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else{
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }
+    });
+
+});
+
+app.get('/sustainability',(req,response)=>
+{
+   fs.readFile('./html/our goal/sustainability.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else{
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }
+    });
+
+});
+
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
