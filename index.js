c6622e0eb33bf920bdab379bb9d7babcbcb8d188
Nabeel hussain shah <hamdani.no02@gmail.com>
2020-02-23 15:16:59 +0500

    second commit

--- index.js
+++ index.js
@@ -20,35 +20,35 @@ app.get('/safety',(req,response)=>
 
 });
 
-app.get('/efficiency',(req,response)=>
-{
-    fs.readFile('./html/our goal/efficiency.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
+// app.get('/efficiency',(req,response)=>
+// {
+//     fs.readFile('./html/our goal/efficiency.html', function (err, html) {
+//     if (err) {
+//         throw err;
+//     }
+//     else{
+//         response.writeHeader(200, {"Content-Type": "text/html"});
+//         response.write(html);
+//         response.end();
+//     }
+//     });
 
-});
+// });
 
-app.get('/sustainability',(req,response)=>
-{
-   fs.readFile('./html/our goal/sustainability.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
+// app.get('/sustainability',(req,response)=>
+// {
+//    fs.readFile('./html/our goal/sustainability.html', function (err, html) {
+//     if (err) {
+//         throw err;
+//     }
+//     else{
+//         response.writeHeader(200, {"Content-Type": "text/html"});
+//         response.write(html);
+//         response.end();
+//     }
+//     });
 
-});
+// });
 
 
 const PORT = process.env.PORT || 5000;
