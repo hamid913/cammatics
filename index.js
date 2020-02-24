35ee1c0e09fa4f675aa7700741c20414e64c1658
Nabeel hussain shah <hamdani.no02@gmail.com>
2020-02-25 01:16:04 +0500

    browse solution nav added

--- index.js
+++ index.js
@@ -50,6 +50,80 @@ app.get('/sustainability',(req,response)=>
 
 });
 
+app.get('/dash-cam',(req,response)=>
+{
+   fs.readFile('./html/browse-solutions/dash-cam.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else{
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }
+    });
+
+});
+
+app.get('/eld-hos',(req,response)=>
+{
+   fs.readFile('./html/browse-solutions/eld-hos.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else{
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }
+    });
+
+});
+
+app.get('/oil-gas',(req,response)=>
+{
+   fs.readFile('./html/browse-solutions/oil-gas.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else{
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }
+    });
+
+});
+
+app.get('/drivers-training',(req,response)=>
+{
+   fs.readFile('./html/browse-solutions/drivers-training.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else{
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }
+    });
+
+});
+
+app.get('/dvir',(req,response)=>
+{
+   fs.readFile('./html/browse-solutions/dvir.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    else{
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }
+    });
+
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
