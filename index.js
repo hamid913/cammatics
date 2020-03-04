79d5066bda9a397c83cef2257de4222f2d4e2160
Nabeel hussain shah <hamdani.no02@gmail.com>
2020-03-04 17:08:45 +0500

    reset

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/safety',(req,response)=>
+app.get('/',(req,response)=>
 {
-    fs.readFile('./html/our goal/safety.html', function (err, html) {
+    fs.readFile('./html/home-2.html', function (err, html) {
         if (err) {
             throw err;
         }
@@ -20,110 +20,6 @@ app.get('/safety',(req,response)=>
 
 });
 
-app.get('/efficiency',(req,response)=>
-{
-    fs.readFile('./html/our goal/efficiency.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
-
-});
-
-app.get('/sustainability',(req,response)=>
-{
-   fs.readFile('./html/our goal/sustainability.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
-
-});
-
-app.get('/dash-cam',(req,response)=>
-{
-   fs.readFile('./html/browse-solutions/dash-cam.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
-
-});
-
-app.get('/eld-hos',(req,response)=>
-{
-   fs.readFile('./html/browse-solutions/eld-hos.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
-
-});
-
-app.get('/oil-gas',(req,response)=>
-{
-   fs.readFile('./html/browse-solutions/oil-gas.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
-
-});
-
-app.get('/drivers-training',(req,response)=>
-{
-   fs.readFile('./html/browse-solutions/drivers-training.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
-
-});
-
-app.get('/dvir',(req,response)=>
-{
-   fs.readFile('./html/browse-solutions/dvir.html', function (err, html) {
-    if (err) {
-        throw err;
-    }
-    else{
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-    }
-    });
-
-});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
