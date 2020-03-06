38571bdbb84582244a2ade8ca8d4846c4048fc7b
Nabeel hussain shah <hamdani.no02@gmail.com>
2020-03-07 02:16:46 +0500

    snowlake

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ const fs = require('fs');
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/',(request,response)=>
+app.get('/safety',(request,response)=>
 {
-    fs.readFile('./html/home-2.html', function (err, html) {
+    fs.readFile('./html/improve-safety.html', function (err, html) {
         if (err) {
             throw err;
         }
@@ -20,9 +20,9 @@ app.get('/',(request,response)=>
 
 });
 
-app.all("*",(request,response)=>{
-
-    fs.readFile('./html/page-404-3.html', function (err, html) {
+app.get('/ensure',(request,response)=>
+{
+    fs.readFile('./html/ensure-competency.html', function (err, html) {
         if (err) {
             throw err;
         }
@@ -34,5 +34,6 @@ app.all("*",(request,response)=>{
         });
 
 });
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
